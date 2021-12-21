3ffc0b252bbddcb2d3af5b6c6304662794b0a85c
cristianvaquero <cristianvaquerodcv@gmail.com>
2021-12-21 17:39:16 -0300

    Primer commit de Api Pokemon

--- ajax.js
+++ ajax.js
@@ -1,5 +1,5 @@
 const APIURL = `https://pokeapi.co/api/v2/pokemon/`;
-const pokeId = $('pokemonId')
+const pokeId = $('#pokemonId')
 
 const renderData = (pokemon) => {
     const pokedex = $('#pokedex');
@@ -7,7 +7,12 @@ const renderData = (pokemon) => {
         <div class="card" style="width: 18rem;">
             <img src="${pokemon.sprites.front_default}" class="card-img-top" alt="...">
             <div class="card-body">
-            <h5 class="card-title">Card title</h5>
+                <h5 class="card-title">${pokemon.name}</h5>
+                <ul>
+                    <li>Pokemon Id: ${pokemon.id}</li>
+                    <li>Height: ${pokemon.height}</li>
+                    <li>Weight: ${pokemon.weght}</li>
+                </ul>
             </div>
         </div>
     `);
@@ -19,23 +24,23 @@ const getData = (valorIdPokemon) => {
         url: APIURL + `${valorIdPokemon}`,
         success: (respuesta) => {
             console.log(respuesta);
-            renderData(respuesta);
-
+        renderData(respuesta);
+        },
+        error: () => {
+            alert('Algo salio mal')
         }
     })
 };
 
-// $(document).ready(function(){
-//     getData();
-// });
+$('#renderPokemon').click ( () => {
+    let valorIdPokemon = Number(pokeId[0].value);
+    console.log(valorIdPokemon);
 
-$('#renderPokemon').click( () => {
-    let valorIdPokemon = pokeId[0].value;
-    console.log(valorIdPokemon)
-    getData(valorIdPokemon);
+    if(Number.isInteger(valorIdPokemon) && valorIdPokemon > 0 && valorIdPokemon < 899){
+        getData(valorIdPokemon);
+    }
+    else {
+        alert('Coloca un numero entero entre 1 y 898')
+    }
+    
 })
-
-
-
-
-
